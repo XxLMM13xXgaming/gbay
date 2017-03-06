7d05f8d95da883d2303b4b62904e6d08311ce43d
Matt S <XxLMM13xXgaming@gmail.com>
2017-03-05 22:07:32 -0500

    :notebook: MySQL table creation!

--- lua/gbay/mysql/sv_mysql.lua
+++ lua/gbay/mysql/sv_mysql.lua
@@ -1,5 +1,137 @@
 require("tmysql4")
 local GBayMySQLInfo = sql.Query("SELECT * FROM gbaymysqlinfo")
+local GBayMySQLCreateTables = [[CREATE TABLE IF NOT EXISTS `ads` (
+  `id` int(11) NOT NULL,
+  `asid` varchar(17) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `iid` int(255) NOT NULL,
+  `timetoexpire` int(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `bans` (
+  `id` int(11) NOT NULL,
+  `bid` varchar(17) NOT NULL,
+  `aid` varchar(17) NOT NULL,
+  `time` int(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `entities` (
+  `id` int(11) NOT NULL,
+  `sidmerchant` varchar(17) NOT NULL,
+  `name` varchar(27) NOT NULL,
+  `description` varchar(81) NOT NULL,
+  `ent` varchar(255) NOT NULL,
+  `price` int(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` int(11) NOT NULL,
+  `sidmerchant` varchar(17) NOT NULL,
+  `sidcustomer` varchar(17) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `weapon` varchar(255) NOT NULL,
+  `weaponshipname` varchar(255) NOT NULL,
+  `quantity` int(255) NOT NULL,
+  `pricepaid` int(255) NOT NULL,
+  `timestamp` int(255) NOT NULL,
+  `completed` tinyint(1) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `players` (
+  `id` int(11) NOT NULL,
+  `sid` varchar(17) NOT NULL,
+  `rank` varchar(255) NOT NULL,
+  `positiverep` int(255) NOT NULL,
+  `neutralrep` int(255) NOT NULL,
+  `negativerep` int(255) NOT NULL,
+  `rating` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `serverinfo` (
+  `id` int(11) NOT NULL,
+  `servername` varchar(15) NOT NULL,
+  `ads` tinyint(1) NOT NULL,
+  `services` tinyint(1) NOT NULL,
+  `coupons` tinyint(1) NOT NULL,
+  `feepost` int(255) NOT NULL,
+  `maxprice` int(255) NOT NULL,
+  `taxpercent` int(255) NOT NULL,
+  `ttnoo` int(255) NOT NULL,
+  `npcpos` varchar(255) NOT NULL,
+  `npcang` varchar(255) NOT NULL,
+  `npcmodel` varchar(255) NOT NULL,
+  `ranks` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `service` (
+  `id` int(11) NOT NULL,
+  `sidmerchant` varchar(17) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `description` varchar(81) NOT NULL,
+  `price` int(255) NOT NULL,
+  `buyers` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `shipments` (
+  `id` int(11) NOT NULL,
+  `sidmerchant` varchar(17) NOT NULL,
+  `name` varchar(27) NOT NULL,
+  `description` varchar(81) NOT NULL,
+  `wep` varchar(255) NOT NULL,
+  `wepname` varchar(255) NOT NULL,
+  `price` int(255) NOT NULL,
+  `amount` int(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+ALTER TABLE `ads`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `bans`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `entities`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `players`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `serverinfo`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `service`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `shipments`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `ads`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `bans`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `entities`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `players`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `serverinfo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `service`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `shipments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+]]
+
 if GBayMySQLInfo then
   local GBayMySQLInfo = sql.Query("SELECT * FROM gbaymysqlinfo")
   local GBayMySQLHost = GBayMySQLInfo[1].hostname
@@ -7,6 +139,7 @@ if GBayMySQLInfo then
   local GBayMySQLPassword = GBayMySQLInfo[1].password
   local GBayMySQLDatabase = GBayMySQLInfo[1].database
   local GBayMySQLPort = GBayMySQLInfo[1].port
+
   GBayMySQL, GBayErr = tmysql.initialize(GBayMySQLHost, GBayMySQLUsername, GBayMySQLPassword, GBayMySQLDatabase, GBayMySQLPort, nil, CLIENT_MULTI_STATEMENTS )
   if GBayErr != nil or tostring( type( GBayMySQL ) ) == "boolean" then
     MsgC(Color(255, 255, 255), "[", Color(255, 0, 0), "GBay", Color(255, 255, 255), "] Error connecting to the database...\n")
@@ -19,6 +152,11 @@ if GBayMySQLInfo then
       GBayRefreashSettings()
     end)
   end
+
+  GBayMySQL:Query(GBayMySQLCreateTables, function(createtableresult)
+    if createtableresult[1].status == false then print('GBay MySQL Error: '..createtableresult[1].error) end
+  end)
+
 end
 
 net.Receive("GBaySetMySQL",function(len, ply)
@@ -60,17 +198,20 @@ net.Receive("GBaySetMySQL",function(len, ply)
         net.Send(ply)
       end)
     else
-      GBayMySQL:Query("SELECT * FROM players", function(result)
-        MsgC(Color(255, 255, 255), "[", Color(0, 0, 255, 255), "GBay", Color(255, 255, 255), "] Connected to database... Currently stores "..#result[1].data.." players!\n")
-        net.Start("GBayCloseSetMySQL")
+      GBayMySQL:Query(GBayMySQLCreateTables, function(createtableresult)
+        if createtableresult[1].status == false then print('GBay MySQL Error: '..createtableresult[1].error) end
+        GBayMySQL:Query("SELECT * FROM players", function(result)
+          MsgC(Color(255, 255, 255), "[", Color(0, 0, 255, 255), "GBay", Color(255, 255, 255), "] Connected to database... Currently stores "..#result[1].data.." players!\n")
+          net.Start("GBayCloseSetMySQL")
           net.WriteBool(true)
           net.WriteString("")
-        net.Send(ply)
-        MsgC(Color(255, 255, 255), "[", Color(0, 0, 255, 255), "GBay", Color(255, 255, 255), "] Logging Server...\n")
-        http.Post("http://xxlmm13xxgaming.com/addons/data/serveradd.php",{sid = "gbay", sip = game.GetIPAddress(), sdate=tostring(os.time()), soid = ply:SteamID64()},function(body)
-          print(body)
-        end,function(error)
-          print(error)
+          net.Send(ply)
+          MsgC(Color(255, 255, 255), "[", Color(0, 0, 255, 255), "GBay", Color(255, 255, 255), "] Logging Server...\n")
+          http.Post("http://xxlmm13xxgaming.com/addons/data/serveradd.php",{sid = "gbay", sip = game.GetIPAddress(), sdate=tostring(os.time()), soid = ply:SteamID64()},function(body)
+            print(body)
+          end,function(error)
+            print(error)
+          end)
         end)
       end)
     end
@@ -82,5 +223,6 @@ concommand.Add("gbayresetmysql",function(ply)
     sql.Query("DROP TABLE gbaymysqlinfo")
     GBayMySQLInfo = sql.Query("SELECT * FROM gbaymysqlinfo")
     print(GBayMySQLInfo)
+    ply:ChatPrint("Done")
   end
 end)
